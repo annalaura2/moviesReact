7320116c17ef57cf75c500b0dbda300b14d20cf3
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-04-01 22:21:46 -0300

    Create index.js

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import styles from "./Footer.module.css"
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <h2>
+                Movies &copy; desenvolvido por Anna Laura
+            </h2>
+        </footer>
+
+    );
+}
+
+export default Footer;
