322fe1ea5abe90b6319c21dc7835cf19e9705613
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-04-01 22:20:58 -0300

    Create index.js

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import styles from "./Header.module.css"
+
+export function Header() {
+
+    return (
+        <header className={styles.header}>
+            <span>
+                Movies
+            </span>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Assistir</a>
+            </nav>
+        </header>
+    );
+}
+
+
+export default Header;
