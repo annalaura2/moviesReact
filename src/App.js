c23fed9c672ff0094fa0d3ad68a68b9b882023f7
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-04-01 22:22:18 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer"
+
+
+function App() {
+  return (
+    <> 
+    <Header />
+      <h1> projeto - filmes </h1>
+      <p> meu primeiro projeto com react</p>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
